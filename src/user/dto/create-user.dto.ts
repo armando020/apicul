3bdb18c26d6e56d6dca4b4ab153612f8bcbc1b3d
armando020 <armandomolina030@gmail.com>
2024-05-10 23:14:45 -0500

    class validator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { isEmail, isNotEmpty, maxLength, minLength } from "class-validator";
+
+export class CreateUserDto {
+    /*@isEmail()
+    email: string 
+    @minLength(10,{
+        message: "minimo 10 caracteres"
+    })
+    @maxLength(15,{
+        message: "maximo 15 caracteres"
+    })
+    @isNotEmpty()
+    firstName: string */
+}
